9ccbc6c397d78afaf21cc2b064c901ff021cfa3d
Tomi <pintertamas99@gmail.com>
2022-11-24 23:25:57 +0100

    kubernetes deployment config

--- docker-script.bat
+++ docker-script.bat
@@ -1,67 +1,66 @@
-
 pwd
-echo 'Cleaning up...'
+echo Cleaning up...
 docker system prune --all -f
 docker container prune -f
 docker volume prune -f
 
-echo 'Creating Docker containers from Dockerfiles...'
+echo Creating Docker containers from Dockerfiles...
 
-echo '--------------------api-gateway--------------------'
+echo --------------------api-gateway--------------------
 cd api-gateway
 call .\mvnw package clean install
 docker build --tag=api-gateway:latest .
 cd ..
 
-echo '--------------------authorization-service--------------------'
+echo --------------------authorization-service--------------------
 cd authorization-service
 call .\mvnw package clean install
 docker build --tag=authorization-service:latest .
 cd ..
 
-echo '--------------------friend-service--------------------'
+echo --------------------friend-service--------------------
 cd friend-service
 call .\mvnw package clean install
 docker build --tag=friend-service:latest .
 cd ..
 
-echo '--------------------interaction-service--------------------'
+echo --------------------interaction-service--------------------
 cd interaction-service
 call .\mvnw package clean install
 docker build --tag=interaction-service:latest .
 cd ..
 
-echo '--------------------naming-server--------------------'
+echo --------------------naming-server--------------------
 cd naming-server
 call .\mvnw package clean install
 docker build --tag=naming-server:latest .
 cd ..
 
-echo '--------------------notification-service--------------------'
+echo --------------------notification-service--------------------
 cd notification-service
 call .\mvnw package clean install
 docker build --tag=notification-service:latest .
 cd ..
 
-echo '--------------------post-service--------------------'
+echo --------------------post-service--------------------
 cd post-service
 call .\mvnw package clean install
 docker build --tag=post-service:latest .
 cd ..
 
-echo '--------------------time-service--------------------'
+echo --------------------time-service--------------------
 cd time-service
 call .\mvnw package clean install
 docker build --tag=time-service:latest .
 cd ..
 
-echo '--------------------user-service--------------------'
+echo --------------------user-service--------------------
 cd user-service
 call .\mvnw package clean install
 docker build --tag=user-service:latest .
 cd ..
 
-echo 'Docker containers created successfully'
+echo Docker containers created successfully
 
-echo 'Running containers'
+echo Running containers
 #docker compose up
