1cdc92535808788c7fd0aab16fc32088de171c07
Tomi <pintertamas99@gmail.com>
2022-11-28 22:07:36 +0100

    kubernetes deployment config update

--- docker-script.bat
+++ docker-script.bat
@@ -9,58 +9,67 @@ echo Creating Docker containers from Dockerfiles...
 echo --------------------api-gateway--------------------
 cd api-gateway
 call .\mvnw package clean install
-docker build --tag=api-gateway:latest .
+docker build --tag=pintertamas/api-gateway:SNAPSHOT-0.0.1 .
+docker push pintertamas/api-gateway:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------authorization-service--------------------
 cd authorization-service
 call .\mvnw package clean install
-docker build --tag=authorization-service:latest .
+docker build --tag=pintertamas/authorization-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/authorization-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------friend-service--------------------
 cd friend-service
 call .\mvnw package clean install
-docker build --tag=friend-service:latest .
+docker build --tag=pintertamas/friend-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/friend-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------interaction-service--------------------
 cd interaction-service
 call .\mvnw package clean install
-docker build --tag=interaction-service:latest .
+docker build --tag=pintertamas/interaction-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/interaction-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------naming-server--------------------
 cd naming-server
 call .\mvnw package clean install
-docker build --tag=naming-server:latest .
+docker build --tag=pintertamas/naming-server:SNAPSHOT-0.0.1 .
+docker push pintertamas/naming-server:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------notification-service--------------------
 cd notification-service
 call .\mvnw package clean install
-docker build --tag=notification-service:latest .
+docker build --tag=pintertamas/notification-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/notification-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------post-service--------------------
 cd post-service
 call .\mvnw package clean install
-docker build --tag=post-service:latest .
+docker build --tag=pintertamas/post-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/post-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------time-service--------------------
 cd time-service
 call .\mvnw package clean install
-docker build --tag=time-service:latest .
+docker build --tag=pintertamas/time-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/time-service:SNAPSHOT-0.0.1
 cd ..
 
 echo --------------------user-service--------------------
 cd user-service
 call .\mvnw package clean install
-docker build --tag=user-service:latest .
+docker build --tag=pintertamas/user-service:SNAPSHOT-0.0.1 .
+docker push pintertamas/user-service:SNAPSHOT-0.0.1
 cd ..
 
 echo Docker containers created successfully
 
 echo Running containers
-#docker compose up
+::docker compose up
